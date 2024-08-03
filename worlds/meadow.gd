add4ae5a4033b092c1e83649ae87edc5ddbb0d6a
emmetl913 <elars1002@gmail.com>
2024-08-03 15:11:23 -0400

    click and drag prototype

--- worlds/meadow.gd
+++ worlds/meadow.gd
@@ -1,15 +1,35 @@
 extends Node2D
 
 @onready var toolbelt_open : bool = false
+var camera_initial_position: Vector2
+var mouse_initial_position: Vector2
+
+@onready var camera = $CursorCamera
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("drag"):
+			camera_initial_position = camera.position
+			mouse_initial_position = event.position
+		if event.is_action_released("drag"):
+			set_process(false)
+	elif event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+			var mouse_diff = event.position - mouse_initial_position
+			camera.position = camera_initial_position - mouse_diff
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CursorCamera/ToolBelt.position.y += 14
+	set_process(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		set_process(true)
+	else:
+		set_process(false)
 
 
 func _on_sunflower_mouse_entered():
