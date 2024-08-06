c970a76875d3946f7e87e937e12b5450ee7f43f0
CiscoMike-git <79672935+CiscoMike-git@users.noreply.github.com>
2024-08-05 22:50:30 -0400

    Spirit Spawn And Base Movement Expanded
    - Added an oscillation factor for spirit movement to make entities appear as if their floating
    - Refactored the static spawning system into a dynamic spawning system
    - Refactored case-like if statements into match statements (optimization/cleanliness)

--- entities/spirits/spirit.gd
+++ entities/spirits/spirit.gd
@@ -2,33 +2,33 @@ extends Entity
 
 enum State { passive, aggressive, vicious }
 
-@export var speed: float
+const Frequency: float = 5
+const dampener: float = .25
+
+@export var speed: float = 5
 
 var state: State
 var phase_offset: float = Function_Lib._random_unit_wave_amplitude() * 90
 
-static var spirit_timer
-
 func _ready() -> void:
-	($"../Day-Night Cycle").time_period_change.connect(_change_behavior)
-	_change_behavior(($"../Day-Night Cycle").time_period)
-	
-	spirit_timer = $"Entity Spawn System/Spirit Timer"
+	$"/root/Meadow/Day-Night Cycle".time_period_change.connect(_change_behavior)
+	_change_behavior($"/root/Meadow/Day-Night Cycle".time_period)
 
 func _process(delta) -> void:
 	move(delta)
 
 func _change_behavior(period: Day_Night_Cycle.Time_Period) -> void:
-	if(period == Day_Night_Cycle.Time_Period.dusk || period == Day_Night_Cycle.Time_Period.dawn):
-		state = State.aggressive
-	elif(period == Day_Night_Cycle.Time_Period.midnight):
-		state = State.vicious
-	else:
-		state = State.passive
+	match period:
+		Day_Night_Cycle.Time_Period.dusk, Day_Night_Cycle.Time_Period.dawn:
+			state = State.aggressive
+		Day_Night_Cycle.Time_Period.midnight:
+			state = State.vicious
+		_:
+			state = State.passive
 
 func move(delta: float):
 	var direction: Vector2 = -global_position.normalized()
-	var offset: Vector2 = direction.orthogonal() * sin(Time.get_unix_time_from_system() + phase_offset)
+	var offset: Vector2 = direction.orthogonal() * sin(Time.get_unix_time_from_system() * Frequency + phase_offset) * dampener
 	
 	move_and_collide((direction + offset).normalized() * delta * speed)
 
--- entities/spirits/spirit_timer.gd
+++ entities/spirits/spirit_timer.gd
@@ -2,15 +2,33 @@ extends Timer
 
 var shortest: float
 var longest: float
+var quantity: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timeout.connect(Entity_Spawn_System._random_timer.bind(self, shortest, longest))
-	#_random_timer(timer: Timer, shortest: float, longest: float)
+	timeout.connect(func(): get_parent()._spawn_entity("res://entities/spirits/spirit.tscn", quantity))
 	
-	#($"../Day-Night Cycle").time_period_change.connect(_change_behavior)
-
+	$"/root/Meadow/Day-Night Cycle".time_period_change.connect(_set_timer)
+	_set_timer($"/root/Meadow/Day-Night Cycle".time_period)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _random_timer() -> void:
+	set_wait_time(RandomNumberGenerator.new().randf_range(shortest, longest))
+
+func _set_timer(period: Day_Night_Cycle.Time_Period) -> void:
+	match period:
+		Day_Night_Cycle.Time_Period.dusk, Day_Night_Cycle.Time_Period.dawn:
+			shortest = 2
+			longest = 3
+			quantity = 2
+		Day_Night_Cycle.Time_Period.midnight:
+			shortest = 0.5
+			longest = 1.5
+			quantity = 3
+		_:
+			shortest = 7.5
+			longest = 10
+			quantity = 1
