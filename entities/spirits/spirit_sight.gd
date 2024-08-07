5c0ca7a3136de00df7b7f90f65aaf8edeef1ae0f
CiscoMike-git <79672935+CiscoMike-git@users.noreply.github.com>
2024-08-07 05:38:35 -0400

    Spirit Attack and Base Collision

--- entities/spirits/spirit_sight.gd
+++ entities/spirits/spirit_sight.gd
@@ -2,25 +2,24 @@ class_name Spirit_Sight
 extends Area2D
 
 var within_sight: Dictionary
-var focus: Node2D
+@onready var focus: Node2D = $/root/Meadow/Tree_Main
 
 func _process(delta):
+	#print(within_sight.size())
+	
 	weight_options()
 
 func _on_body_entered(body):
-	if(body is Animal || body is Base_Plant):
+	if body.get_parent() is Animal || body.get_parent() is Plant:
 		within_sight[body] = 0
 
 func _on_body_exited(body):
-	if(body is Animal || body is Base_Plant):
+	if body.get_parent() is Animal || body.get_parent() is Plant:
 		within_sight.erase(body)
 
-
 func weight_options():
-	pass
-	#if within_sight.is_empty():
-		#focus = $Tree_Main
-		
-	
-	#for key in within_sight:
-		#focus = key
+	if within_sight.is_empty():
+		focus = $/root/Meadow/Tree_Main
+	else: # fix
+		for key in within_sight:
+			focus = key
