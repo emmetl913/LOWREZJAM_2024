4206067827ad3ea3c02389a0ac9e68bd024b281f
emmetl913 <elars1002@gmail.com>
2024-08-08 20:22:36 -0400

    Added music and transitions

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,10 +1,19 @@
 extends Control
 
-
+func _ready():
+	$AnimationPlayer.play("fade_to_normal")
 
 func _on_quit_pressed():
 	get_tree().quit()
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://worlds/meadow.tscn")
+	$Fade.visible = true
+	$AnimationPlayer.play("fade_to_black")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "fade_to_normal":
+		$Fade.visible = not $Fade.visible
+	elif anim_name == "fade_to_black":
+		get_tree().change_scene_to_file("res://worlds/meadow.tscn")
